951784c4a87e246920350895dd43884b453abfaf
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-17 13:08:53 +0300

    form in index.php was changed

--- views/index.php
+++ views/index.php
@@ -4,12 +4,12 @@ echo '<body>';
 include "../modules/system_name.php";
 ?>
     <div class="form_auth_block">
-        <div class="form_auth_block_content">
-          <p class="form_auth_block_head_text">Авторизация</p>
+        <form class="form_auth_block_content">
+            <p class="form_auth_block_head_text">Авторизация</p>
             <input id="login" type="login" name="login" placeholder="Введите логин" required><br>
             <input id="pass" type="password" name="auth_pass" placeholder="Введите пароль" required >
-            <button class="form_auth_button" name="form_auth_submit">Войти</button>
-        </div>
+            <input class="form_auth_button" type="submit" name="form_auth_submit">Войти</input>
+        </form>
         <div class="message"></div>
     </div>
 <?php
