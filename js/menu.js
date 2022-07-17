a4a5b072ac1b33276ac0c354534a42dcaffc3cc8
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-17 17:53:18 +0300

    menu.js changes

--- js/menu.js
+++ js/menu.js
@@ -9,16 +9,15 @@ function button_generator(button_class,button_id, button_onclick, button_value){
 }
 
 function menu_generator(){
-    let top_part=$('.form_auth_block');
+    $('.form_auth_block').remove();
+    let block_up=$('.block_up');
     let container_class = document.createElement('div');
     container_class.setAttribute('id', 'container');
     container_class.className = 'container';
-    top_part.after(container_class);
+    block_up.after(container_class);
     container_class.append(button_generator('menu_button','show_bk','show_books()','Каталог книг'));
     container_class.append(button_generator('menu_button','buy_book','buy_books()','Купить книги у поставщика'));
     container_class.append(button_generator('menu_button','sell_book','sell_books()','Продать книги'));
     container_class.append(button_generator('menu_button','create_a_report','report_page()','Сформировать отчет'));
     container_class.append(button_generator('menu_button','exit','exit()','Выход'));
-    let footer = footer_generator();
-    container_class.after(footer);
 }
