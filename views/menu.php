303939c8dbf6e2cc954e7f24c99443400253ba50
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-19 13:13:16 +0300

    onclick was removed from buttons in menu.php

--- views/menu.php
+++ views/menu.php
@@ -1,9 +1,9 @@
 <?php
 echo '<div id="container" class="container">
 <div class="user">'.$_SESSION["user_name"].'</div>
-<input class="menu_button" id="show_bk" type="button" onclick=" " value="Каталог книг">
-<input class="menu_button" id="buy_book" type="button" onclick=" " value="Купить книги у поставщика">
-<input class="menu_button" id="sell_book" type="button" onclick=" " value="Продать книги">
-<input class="menu_button" id="create_a_report" type="button" onclick=" " value="Сформировать отчет">
-<input class="menu_button" id="exit" type="button" onclick=" " value="Выход"></div>';
+<input class="menu_button" id="show_bk" type="button" value="Каталог книг">
+<input class="menu_button" id="buy_book" type="button" value="Купить книги у поставщика">
+<input class="menu_button" id="sell_book" type="button" value="Продать книги">
+<input class="menu_button" id="create_a_report" type="button" value="Сформировать отчет">
+<input class="menu_button" id="exit" type="button" value="Выход"></div>';
 ?>
