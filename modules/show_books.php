8afef2891fb7d62c1d882d7ed4023d44460a7160
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-19 11:50:53 +0300

    show_books.php was created

---
+++ modules/show_books.php
@@ -0,0 +1,24 @@
+<?php
+include '../db.php';
+$sql = 'SELECT * FROM `books`';
+$result = $connect->query($sql);
+echo '<br>';
+echo '<table>';
+echo '<tr>';
+echo '<th>id</th>';
+echo '<th>Название</th>';
+echo '<th>Автор</th>';
+echo '<th>Год</th>';
+echo '<th>Цена</th>';
+echo '<th>Количество</th>';
+while($row = $result->fetch(PDO::FETCH_ASSOC)){
+    echo '<tr>';
+    echo'<td>'.$row['book_id'].'</td>';
+    echo '<td>'.$row['title'].'</td>';
+    echo'<td>'.$row['author'].'</td>';
+    echo'<td>'.$row['year'].'</td>';
+    echo'<td>'.$row['price'].'</td>';
+    echo'<td>'.$row['quantity'].'</td>';
+    echo '</tr>';
+}
+?>
