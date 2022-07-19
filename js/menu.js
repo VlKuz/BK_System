762d5b020ab9c2bb20f8a4baff2dae74100f85c8
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-19 14:58:54 +0300

    change_title function was created in menu.js

--- js/menu.js
+++ js/menu.js
@@ -44,13 +44,39 @@ $(document).ready(function(){
                     let button_catalog_container_class = document.createElement('div');
                     button_catalog_container_class.className = 'button_catalog_container';
                     return_button.after(button_catalog_container_class);
-                    button_catalog_container_class.append(button_generator('catalog_button','','change_title()','Изменить название'));
-                    button_catalog_container_class.append(button_generator('catalog_button','','change_author()','Изменить автора'));
-                    button_catalog_container_class.append(button_generator('catalog_button','','change_year()','Изменить год'));
-                    button_catalog_container_class.append(button_generator('catalog_button','','change_price()','Изменить цену'));
-                    button_catalog_container_class.append(button_generator('catalog_button','','change_quantity()','Изменить количество'));
+                    button_catalog_container_class.append(button_generator('catalog_button','change_title','','Изменить название'));
+                    button_catalog_container_class.append(button_generator('catalog_button','change_author()','','Изменить автора'));
+                    button_catalog_container_class.append(button_generator('catalog_button','change_year()','','Изменить год'));
+                    button_catalog_container_class.append(button_generator('catalog_button','change_price()','','Изменить цену'));
+                    button_catalog_container_class.append(button_generator('catalog_button','change_quantity()','','Изменить количество'));
                     $(button_catalog_container_class).after('<div class = "books"></div>');
                     $('.books').html(data);
+                }else
+                    alert('error');
+            }
+        });
+    });
+
+    $(document).on('click','#change_title', function(){
+        let id = prompt('Введите id книги:');
+        let new_title = prompt('Введите новое название книги:');
+        $.ajax({
+            url: '../modules/change_title.php',
+            method: 'POST',
+            data: {book_id: id, new_title: new_title},
+            success: function(data){
+                if(data){
+                    $.ajax({
+                        url: '../modules/show_books.php',
+                        method: 'POST',
+                        data: 'books',
+                        success: function(data){
+                            if(data){
+                                $('.books').html(data);
+                            }else
+                                alert('error');
+                        }
+                    });
                 }else
                     alert('error');
             }
