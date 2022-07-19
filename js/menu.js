8c504dd2a59ec6658eed0d063bd98299b504d386
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-19 13:34:01 +0300

    sell_book function was added in menu.js

--- js/menu.js
+++ js/menu.js
@@ -76,6 +76,28 @@ $(document).ready(function(){
         });
     });
 
+    $(document).on('click','#sell_book', function(){
+        $.ajax({
+            url: '../modules/show_books.php',
+            method: 'POST',
+            data: 'books',
+            success: function(data){
+                if(data){
+                    $('#container').remove();
+                    let return_button = button_generator('return_button','return_button',' ','Назад');
+                    $('#block_up').after(return_button);
+                    let button_catalog_container_class = document.createElement('div');
+                    button_catalog_container_class.className = 'button_catalog_container';
+                    return_button.after(button_catalog_container_class);
+                    button_catalog_container_class.append(button_generator('catalog_button','','sell_shop_book()','Продать книгу'));
+                    $(button_catalog_container_class).after('<div class = "books"></div>');
+                    $('.books').html(data);
+                }else
+                    alert('error');
+            }
+        });
+    });
+
     $(document).on('click','#exit', function(){
         $.ajax({
             url: '../modules/exit.php',
