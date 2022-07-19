925a78af0094468909e7ff3af95a3a3723690535
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-19 09:30:25 +0300

    button_generator was deleted

--- js/button_generator.js
+++
@@ -1,9 +0,0 @@
-function button_generator(button_class,button_id, button_onclick, button_value){
-    let button = document.createElement('input');
-    button.className = button_class;
-    button.setAttribute('id', button_id);
-    button.setAttribute('type', 'button');
-    button.setAttribute('onclick', button_onclick);
-    button.setAttribute('value', button_value);
-    return button;
-}
