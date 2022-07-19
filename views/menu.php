e9f4b8390e6d4221d09598fab69693beaf11fdf4
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-19 13:01:56 +0300

    menu.php was created

---
+++ views/menu.php
@@ -0,0 +1,9 @@
+<?php
+echo '<div id="container" class="container">
+<div class="user">'.$_SESSION["user_name"].'</div>
+<input class="menu_button" id="show_bk" type="button" onclick=" " value="Каталог книг">
+<input class="menu_button" id="buy_book" type="button" onclick=" " value="Купить книги у поставщика">
+<input class="menu_button" id="sell_book" type="button" onclick=" " value="Продать книги">
+<input class="menu_button" id="create_a_report" type="button" onclick=" " value="Сформировать отчет">
+<input class="menu_button" id="exit" type="button" onclick=" " value="Выход"></div>';
+?>
