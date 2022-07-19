5b253b6e87214a7f740a5600cd4cc7ae3d3303fb
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-19 14:05:10 +0300

    create_a_report.php was created

---
+++ modules/create_a_report.php
@@ -0,0 +1,9 @@
+<?php
+echo '<br><div class="report_container" id="report_container">
+<div class="report_item"><p>Денежные средства: 100000</p></div>
+<div class="report_item"><p>Куплено книг: 0</p></div>
+<div class="report_item"><p>Продано книг: 0</p></div>
+<div class="report_item"><p>Куплено книг на сумму: 0</p></div>
+<div class="report_item"><p>Продано книг на сумму: 0</p></div>
+<div class="report_item"><p>Прибыль: 0</p></div>
+</div>';
