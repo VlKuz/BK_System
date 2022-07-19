108b84aae0e189d9bbf401d7f28ff219e4f30e04
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-19 10:07:55 +0300

    index.php changes

--- views/index.php
+++ views/index.php
@@ -1,18 +1,23 @@
 <?php
+session_start();
 include 'header.php';
 echo '<body>';
-include 'system_name.php';
-?>
-    <div class="form_auth_block">
-        <form id="auth_from" class="form_auth_block_content">
-            <p class="form_auth_block_head_text">Авторизация</p>
-            <input id="login" type="login" name="login" placeholder="Введите логин" required><br>
-            <input id="pass" type="password" name="password" placeholder="Введите пароль" required >
-            <button class="form_auth_button" name="form_auth_submit">Войти</button>
-        </form>
-        <div class="message"></div>
-    </div>
-<?php
+if(isset($_SESSION["user_name"])){
+    include 'system_name.php';
+    echo '<div class="user">'.$_SESSION["user_name"].'</div>';
+    echo '<script>menu_generator()</script>';
+}else{
+    include 'system_name.php';
+    echo '<div class="form_auth_block">
+    <form id="auth_form" class="form_auth_block_content">
+        <p class="form_auth_block_head_text">Авторизация</p>
+        <input id="login" type="login" name="login" placeholder="Введите логин" required><br>
+        <input id="pass" type="password" name="password" placeholder="Введите пароль" required >
+        <button class="form_auth_button">Войти</button>
+    </form>
+    <div class="message"></div>
+</div>';
+} 
 include 'footer.php';
 ?>
 </body>
