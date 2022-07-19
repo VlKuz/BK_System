00a0ce232b295fc3237b1d830e08e41208512edc
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-07-19 09:34:50 +0300

    functions for show books and exit were created in menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,50 @@
+function button_generator(button_class,button_id, button_onclick, button_value){
+    let button = document.createElement('input');
+    button.className = button_class;
+    button.setAttribute('id', button_id);
+    button.setAttribute('type', 'button');
+    button.setAttribute('onclick', button_onclick);
+    button.setAttribute('value', button_value);
+    return button;
+}
 function menu_generator(){
     $('.form_auth_block').remove();
-    let block_up=$('.block_up');
-    let container_class = document.createElement('div');
-    container_class.setAttribute('id', 'container');
-    container_class.className = 'container';
-    block_up.after(container_class);
-    container_class.append(button_generator('menu_button','show_bk','show_books()','Каталог книг'));
-    container_class.append(button_generator('menu_button','buy_book','buy_books()','Купить книги у поставщика'));
-    container_class.append(button_generator('menu_button','sell_book','sell_books()','Продать книги'));
-    container_class.append(button_generator('menu_button','create_a_report','report_page()','Сформировать отчет'));
-    container_class.append(button_generator('menu_button','exit','exit()','Выход'));
+    let container_class = $('.container');
+    container_class.append(button_generator('menu_button','show_bk',' ','Каталог книг'));
+    container_class.append(button_generator('menu_button','buy_book',' ','Купить книги у поставщика'));
+    container_class.append(button_generator('menu_button','sell_book',' ','Продать книги'));
+    container_class.append(button_generator('menu_button','create_a_report',' ','Сформировать отчет'));
+    container_class.append(button_generator('menu_button','exit',' ','Выход'));
 }
+
+$(document).ready(function(){
+
+    $(document).on('click','#show_bk', function(){
+        $.ajax({
+            url: '../modules/exit.php',
+            method: 'POST',
+            data: 'exit_data',
+            success: function(data){
+                if(data==1)
+                    window.location.reload();
+                else
+                    alert('error');
+            }
+        });
+    });
+
+    $(document).on('click','#exit', function(){
+        $.ajax({
+            url: '../modules/exit.php',
+            method: 'POST',
+            data: 'exit_data',
+            success: function(data){
+                if(data==1)
+                    window.location.reload();
+                else
+                    alert('error');
+            }
+        });
+    });
+    
+});
